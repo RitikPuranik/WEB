8df4a9314b34d63d426873f2d35121233c14aa31
RitikPuranik <ritkipuranik28@gmail.com>
2025-01-11 19:42:34 +0530

    js

---
+++ lec 17/index.js
@@ -0,0 +1,34 @@
+// console.log('heheheh');
+// Number
+// a=5.5
+// String 'c' 'cc' "ccc"
+// Boolean true false 0 1
+// null
+// undefined
+// Symbol
+// BigInt 
+
+// var.let,const
+// var a=5
+// console.log(a)
+// var a=10
+// a=100
+
+// let b=7
+// b=10
+// console.log(b)
+
+// const c=100
+// console.log(c)
+
+var a=100
+var to=a==100
+console.log(to);
+
+let a='rahul'
+let b='jain'
+console.log(`${a}   ${b}`);
+
+// = == ===
+
+console.log(false=='false');
