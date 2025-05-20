f7734eb042fe2f5f610e39cada6a7159badd11ac
RitikPuranik <ritikpuranik28@gmail.com>
2025-05-20 22:04:41 +0530

    js revison

---
+++ rev lec 1/index.js
@@ -0,0 +1,105 @@
+// console.log(5+5);//10
+// console.log(5+"5");//"55"
+// console.log(5-5);//0
+// console.log(5-"5");//0
+// console.log(5*5);//25
+// console.log(5/"5");//1
+// console.log(5%5);//0
+// console.log(5**5);//3125
+// console.log(5**0);//1
+// console.log(5**1);//5
+// console.log(5**2);//25
+// console.log(5**3);//125
+// console.log(5**4);//625
+// console.log(5**5);//3125
+// console.log(5**6);//
+
+// console.log(true && false && true && true);
+// console.log(true || false || true || true );
+// console.log(false && true);
+
+
+// console.log(5&4);
+//5=> 1 0 1
+//4=> 1 0 0     intersection of 1 and 0 is 0
+//    1 0 0
+ // let isBoolean=false
+// if(5&0){
+//     console.log("hello");
+// }
+// else{
+//     console.log("hiiiii");
+// }
+
+// if(""){
+//     console.log("hello");
+// }
+// else{
+//     console.log("hiiiii");
+// }
+
+// if(" "){
+//     console.log("hello");
+// }
+// else{
+//     console.log("hiiiii");
+// }
+//"" and "  " works differntly as "  "contains space as string which is true
+
+
+// let num=prompt("enter a number")
+// if(num%2==0){
+//     console.log("even");
+// }
+// else{
+//     console.log("odd");
+// }
+
+
+// let a=67
+// let m=50
+// if(a>70 && m>75){
+//     console.log("A");
+// }
+// else if(a>70 || m>75){
+//     console.log("B");
+// }
+// else{
+//     console.log("C");
+// }
+// let isBool=true;
+// isBool?console.log("hellooo"):console.log("hiiiii");
+
+
+//console.log(5-"hello");
+//nan
+
+// Object , Array
+// let name1="aanya"
+// let name2="afroz"
+// let name3="ritik"
+
+
+// let student ={
+//     name1:"aanya",
+//     name2:"afroz",
+//     name3:"ritik",
+//     age:35
+// }
+// student.age=20
+// console.log(student.name1);
+// console.log(student.name2);
+// console.log(student.name3);
+// console.log(student.age);
+
+
+
+
+console.log({} == {});
+//{}=>stands for false
+//
+let a={name:"ritik"}
+let b={name:"ritik"}
+console.log(a==b);
+//false
+console.log(a===b);
