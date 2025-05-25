e0291946184daed5107ca5e075d3bb892890d47a
RitikPuranik <ritikpuranik28@gmail.com>
2025-05-25 14:40:21 +0530

    homework js

--- assignment(js)/reduce.js
+++ assignment(js)/reduce.js
@@ -1,7 +1,23 @@
-
+//COUNT THE FREQUENCY OF EACH ELEMENT IN ARRAY
 let arr=["a","b","a","c","b"]
+let frequency=[]
+let count =0;
 let data =arr.reduce((a,b)=>{
-})
+    if(frequency.includes(b)){
+    }
+    else{
+        frequency.push(b)
+        for(let i=0;i<arr.length;i++){
+            if(b==arr[i]){
+                count++
+            }
+        }
+        frequency.push(count);
+        count=0;
+    }
+    return frequency
+},[])
+console.log(data)
 
 
 //TO PRINT THE CONCATENATION OF GIVEN ARRAY
@@ -20,7 +36,18 @@ let data2 =arr2.reduce((a,b)=>{
 console.log(data2/arr2.length)
 
 
+//TO PRINT THE VOWEL COUNT IN GIVEN ARRAY
 let arr3=["apple","banana"]
+let vowel=['a','e','i','o','u']
+let output=[]
 let data3=arr3.reduce((a,b)=>{
-
-})
+    let count=0
+    for(let i=0;i<b.length;i++){
+        if(vowel.includes(b[i])){
+            count++
+        }
+    }
+    output.push(b+"=>"+count)
+    return output
+},0)
+console.log(data3)
