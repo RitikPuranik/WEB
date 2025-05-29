79e1ee345f22bb60970da9277af889c76be7f726
RitikPuranik <ritikpuranik28@gmail.com>
2025-05-29 21:33:10 +0530

    scope

---
+++ js lec 10/index.js
@@ -0,0 +1,94 @@
+// console.log(a);
+// var a=5   // undefined
+// console.log(b);
+// let b=10    // error
+// sum()
+// function sum(){
+//     console.log("hello");
+    
+// }
+
+// fun1()
+// var  fun1=()=>{
+//     console.log("hiii");
+    
+// }
+
+
+// var a = 1;
+// function foo() {
+//   console.log(a);
+//   var a = 2;
+// }
+// foo()
+
+
+
+// var a = 1;
+// function outer() {
+//   console.log(a);
+//   var a = 2;
+//   function inner() {
+//     console.log(a);
+//   }
+//   inner();
+// }
+// outer();
+
+
+
+
+// hoistTest();
+// var hoistTest = function () {
+//   console.log("Hoisted?");
+// };
+
+
+// function foo(){
+//     function bar(){
+//         return 3
+//     }
+//     return bar()
+//     function bar(){
+//         return 8
+//     }
+// }
+// console.log(foo());
+
+
+// if(false){
+//     var a=5
+// }
+// console.log(a);
+
+// {
+//    console.log("helloo");
+    
+// }
+// var a=10
+// {
+//     // let a=10;
+//     // console.log(a);
+//     var a=10
+    
+// }
+// console.log(a);
+
+// function sum(){
+//     let a=1
+// }
+// sum()
+// console.log(a);
+// let a=5
+
+
+if(false){
+    let a=5
+}
+console.log(a);
+//IN BLOG WE CANNOT ACCESS LET AND CONST VARIABLE OUTSIDE ITS SCOPE
+//IN FUNCTION WE CANNOT ACCESS ANY TYPE OF VARIABLE OUTSIDE ITS SCOPE
+//IT WILL SHOW "ERROR:THE VARIABLE CANNOT BE ACCESSED WITHOUT INTIALIZATION"
+//BLOG
+//{...
+//}
