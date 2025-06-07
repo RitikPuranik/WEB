d8221a7a9cc9ae8861e638f809962cc76da186ba
Ritik Puranik <ritikpuranik28@gmail.com>
2025-06-07 21:28:38 +0530

    apply bind call

--- js lec 14/index.js
+++ js lec 14/index.js
@@ -14,9 +14,9 @@ let arr=[1,2,3,4,5,5]
 // }
 // let data=  arr.myFilter(  (a,b,c)=>{
 //     return a==5
-
 // }   )
 // console.log(data);
+
 
 
 // let data=  arr.filter((a,b,c)=>{
@@ -24,6 +24,8 @@ let arr=[1,2,3,4,5,5]
 
 // })
 // console.log(data);
+
+
 
 
 // let myFilter=function(cb){
@@ -36,15 +38,13 @@ let arr=[1,2,3,4,5,5]
 //     }
 //     console.log("hii");
     
-    
-    
-   
-    
 // }
 // myFilter( (a)=>{ 
 //     return a==6
 
 // } )
+
+
 
 
 //find
@@ -69,3 +69,22 @@ let sum= arr1.myReduce(   (a,b)=>{
 
 },0)
 console.log(sum);
+
+
+
+
+
+// forEach
+let arr2=[1,2,3,4,5,6]
+Array.prototype.myforEach=function(cb){
+    for(var i=0;i<this.length;i++){
+      var data=  cb(this[i],i,this)
+       res.push(data)
+        
+    }
+    return res
+}
+
+let print=arr2.myforEach( (a)=>{
+    return a
+} )
