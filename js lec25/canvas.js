be53dba961c3e56df9480da9b3648a47daa61670
Ritik Puranik <ritikpuranik28@gmail.com>
2025-06-26 21:00:00 +0530

    snakegame and canvass

---
+++ js lec25/canvas.js
@@ -0,0 +1,28 @@
+let canvas = document.querySelector('canvas');
+let pen= canvas.getContext('2d')
+
+let snakeCells=[[0,0]]
+let cell=50;
+
+document.addEventListener("keydown",()=>{
+    console.log("keydown")
+})
+
+function draw() {
+    for(let i of snakeCells){
+        pen.fillRect(i[0],i[1],cell,cell)
+    }
+}
+
+
+function update(){
+    let headX=snakeCells[snakeCells.length-1][0];
+    let headY=snakeCells[snakeCells.length-1][1];
+    let newX=headX+cell
+    let newY=headY
+    snakeCells.push([newX,newY])
+}
+setInterval(()=>{
+    draw()
+    update()
+},200)
