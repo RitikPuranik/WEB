47b4122d0b65911cbaad46e87e9dad1d9d9e6209
Ritik Puranik <ritikpuranik28@gmail.com>
2025-07-04 20:34:54 +0530

    api

---
+++ js lec 27/script.js
@@ -0,0 +1,32 @@
+// fetch('https://jsonplaceholder.typicode.com/todos').then((res)=>{
+//     return res.json()
+// }).then((data)=>{
+//     // console.log(data)
+//     data.map((result)=>{
+//         console.log(result);
+//         showData(result)
+//     })
+// })
+
+
+// let div=document.querySelector('div')
+
+// function showData(res){
+//     let li =document.createElement('li')
+//     let h3 =document.createElement('h3')
+//     li.innerText=res.title
+//     h3.innerText=res.id
+//     div.append(h3,li)
+//     h3.style.color='red'
+// }
+
+let city ="delhi"
+let key="9f6290d6cda9a36a63755fadee71f83d";
+let ApiData=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
+
+fetch(ApiData).then((res)=>{
+    return res.json()
+}).then((data)=>{
+    console.log(data)
+    
+})
