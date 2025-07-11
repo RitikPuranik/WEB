c5c33b599b3fda21dfa260e4f82d9b5c1d6ab92d
Ritik Puranik <ritikpuranik28@gmail.com>
2025-07-11 20:44:47 +0530

    react

--- react lec 31/my-react-app/src/App.jsx
+++ react lec 31/my-react-app/src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  function fun(){
+    console.log('hello')
+  }
+
+  return(
+    <div>
+      <button onClick={fun}>click me</button>
+    </div>
   )
+
+
+
+
+  // let arr=[1,2,3,4,5]
+  // return (
+  //   <div>App
+  //     {
+  //       arr.map((a,b,c)=>{
+  //         return(<>
+  //           <h1>{a}</h1>
+  //         </>)
+  //       })
+  //     }
+      
+  //      </div>
+  // )
+
+
+
 }
 
+
+// const Home = () => {
+//   return (
+//     <div>Home</div>
+//   )
+// }
+
+
+// export {Home}
 export default App
--- react lec 31/my-react-app/src/main.jsx
+++ react lec 31/my-react-app/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+// import './index.css'
 import App from './App.jsx'
+// import { Home } from './App.jsx'
+import Home1 from './Home1.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Home1 />
   </StrictMode>,
 )
