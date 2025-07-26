0fa216c73c9b28ea3b721d6ae616b034a18e19e8
Ritik Puranik <ritikpuranik28@gmail.com>
2025-07-26 06:50:07 +0530

    react

--- react lec 31/my-react-app/src/Day5.jsx
+++ react lec 31/my-react-app/src/Day5.jsx
@@ -1,35 +1,47 @@
-import React, { useEffect, useState } from 'react';
-
+import React, { useEffect, useState } from 'react'
+import './Day5.css'
 const Day5 = () => {
-  const [count, setcount] = useState(0);
-  const [input, setinput] = useState([]);
 
-  useEffect(() => {
-    console.log("useEffect");
-    let apiLink = 'https://dummyjson.com/recipes';
-    fetch(apiLink)
-      .then((res) => res.json())
-      .then((d) => {
-        let filterData = d.recipes.filter((apiData) => {
-          return apiData.cuisine === 'Italian';
-        });
-        let names = filterData.map((apiData) => apiData.name);
-        setinput(names);
-      });
-  }, []);
+    let [input ,SetInput]=useState([]);
+    useEffect(()=>{
+        // console.log("hello");
+        fetch('https://jsonplaceholder.typicode.com/todos').
+        then((res)=>{
+            return res.json()
 
+        }).then((data)=>{
+            SetInput(data)
+            console.log(data);
+            
+        })
+        
+    },[])
+// index=3
+    function d(index){
+       let nerArr=   input.filter((a)=>{
+            return a.id!=index
+
+        })
+        SetInput(nerArr)
+        // console.log(nerArr, "heheheheh");
+    }
+    
   return (
     <div>
-      <button onClick={() => setcount(count + 1)}> count {count}</button>
-      {
-        input.map((a, i) => {
-          return (
-            <p key={i}>{a}</p>
-          );
-        })
-      }
+        {/* <button onClick={()=>SetCount(count+1)}> count {count}
+        </button> */}
+        <div id='parent_Card'>  
+        {
+            input.map((a,index)=>{
+                return(<div id='card'  onClick={()=>d(a.id)} >
+                <h1>{a.id}</h1>
+                <button>add</button>
+                </div>)
+            })
+        }
+         </div>
     </div>
-  );
-};
+  )
+}
 
-export default Day5;
+export default Day5
