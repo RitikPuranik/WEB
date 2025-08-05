4a413430207fc9453731a7f6904795dc2f7f46a8
Ritik Puranik <ritikpuranik28@gmail.com>
2025-08-05 20:26:25 +0530

    project

--- react lec 31/my-react-app/src/App.jsx
+++ react lec 31/my-react-app/src/App.jsx
@@ -1,51 +1,153 @@
+// import React from 'react'
+// import { Routes } from 'react-router-dom'
+// import { Link, Route } from 'react-router-dom'
+// import Day1 from './Day1'
+// import Day2 from './Day2'
+// import Day3 from './Day3'
+// import Day4 from './Day4'
+// import Day5 from './Day5'
+
+// import NavBar from './Navbar'
+// import Home from './Home'
+// import Contact from './Contact'
+
+// import Login from './Login'
+// import Signup from './Signup'
+
+// import Day7 from './Day7'
+// import Day8 from './Day8'
+// import Day9 from './Day9'
+
+// const App = () => {
+//   return (
+//     <div>
+//         {/* <Day1 /> */}
+//         {/* <Day2 /> */}
+//         {/* <Day3 /> */}
+//         {/* <Day4 /> */}
+//         {/* <Day5 /> */}
+
+//         {/* <NavBar/>
+//         <Routes>
+//           <Route path="/" element={<Home/>}/>
+//           <Route path='/Contact' element={<Contact/>}/>
+//         </Routes> */}
+
+//         {/* <Routes>
+//           <Route path='/Login' element={<Login/>} />
+//           <Route path='/Signup' element={<Signup/>} />
+//         </Routes> */}
+
+//         {/* <Day7 /> */}
+
+//         {/* <Day8 /> */}
+
+//         {/* <Day9 /> */}
+
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+// import React from 'react'
+// import './App.css'
+
+
+// const App = () => {
+//   let arr=[1,2,3,4,5] 
+//   function fun1(){
+//     console.log("hehehehe");
+    
+//   }
+//   return (
+//     // <div>
+//     // <button  onClick={fun1}>click</button>
+//     // </div>
+//     <div>App 
+//       {
+//         arr.map((a)=>{
+//           return(<>
+//           <h1> {a}</h1>
+//           </>)
+//         })
+//       }
+//     </div>
+//   )
+// }
+
+
+// // const Home=()=>{
+// //   return(
+// //     <div>
+// //       <h2> Home Compoo</h2>
+// //     </div>
+// //   )
+
+// // }
+// // export {Home}
+
+
+// export default App
+
+
+  // props
+
+// import React from 'react'
+// import Day4 from './Day4'
+// import Day5 from './Day5'
+// // import Day2 from './Day2'
+// // import Day3 from './Day3'
+
+// const App = () => {
+//   let user="hello"
+//   return (
+//     <div>
+//       {/* <Day2/> */}
+//       {/* <Day3/> */}
+//       {/* <Day4    data={user} /> */}
+//       <Day5/>
+
+
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+
+
 import React from 'react'
-import { Routes } from 'react-router-dom'
-import { Link, Route } from 'react-router-dom'
-import Day1 from './Day1'
-import Day2 from './Day2'
-import Day3 from './Day3'
-import Day4 from './Day4'
+import NavBar from './NavBar'
+import { Route, Routes } from 'react-router-dom'
 import Day5 from './Day5'
-
-import NavBar from './Navbar'
-import Home from './Home'
-import Contact from './Contact'
-
-import Login from './Login'
-import Signup from './Signup'
-
-import Day7 from './Day7'
-import Day8 from './Day8'
-import Day9 from './Day9'
-
+import Cart from './Cart'
 const App = () => {
+  let [input ,SetInput]=useState([]);
+
   return (
     <div>
-        {/* <Day1 /> */}
-        {/* <Day2 /> */}
-        {/* <Day3 /> */}
-        {/* <Day4 /> */}
-        {/* <Day5 /> */}
-
-        {/* <NavBar/>
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path='/Contact' element={<Contact/>}/>
-        </Routes> */}
-
-        {/* <Routes>
-          <Route path='/Login' element={<Login/>} />
-          <Route path='/Signup' element={<Signup/>} />
-        </Routes> */}
-
-        {/* <Day7 /> */}
-
-        {/* <Day8 /> */}
-
-        <Day9 />
-
+      {/* <Day7/> */}
+      {/* <Day8/> */}
+      {/* <Day9/> */}
+      {/* <Day10/> */}
+      <Cart   />
+      <Day5 input={input}   SetInput={SetInput}/>
+      {/* <NavBar/>
+      <Routes>
+        <Route  path="/" element={<Home/>}/>
+        <Route path='/contact'  element={<Contact/>}/>
+      </Routes> */}
     </div>
   )
 }
 
 export default App
+
+
+
+//npm i react-router-dom
---
+++ react lec 31/my-react-app/src/Dummy.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Dummy = () => {
+  
+  localStorage.removeItem('key')
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Dummy
