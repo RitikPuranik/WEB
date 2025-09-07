d829c576eed85dc0f0476ac6b0cab6253f576a9c
Ritik Puranik <ritikpuranik28@gmail.com>
2025-09-07 06:34:05 +0530

    backend

--- backend/index.js
+++ backend/index.js
@@ -79,3 +79,18 @@
 
 
 
+
+
+
+
+
+
+let http = require('http');
+
+let server = http.createServer(function(req,res){
+    res.end("hello from server")
+})
+
+server.listen(3000, function(){
+    console.log("Server is running on port 3000")
+})
